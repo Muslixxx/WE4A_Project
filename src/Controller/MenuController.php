d1a81fbbcbcf9c3838483121d12d1fde0ff763c4
Lucas Vigny <Lucas.vigny@gmail.com>
2025-04-03 17:56:40 +0200

    ajout de la page d'accueil

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -6,13 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class MenuController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/menu', name: 'app_menu')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('menu.html.twig');
     }
 }
-
-
