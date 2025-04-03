58098e762e8aa651c679b05cf124c2036fc1211f
Ivann <ivann.vasic@utbm.fr>
2025-04-03 16:11:24 +0200

    page se connecter

--- public/index.php
+++ public/index.php
@@ -1,31 +1,9 @@
-{% extends 'base.html.twig' %}
+<?php
 
-{% block body %}
-<div class="row justify-content-center mt-5">
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header text-center">
-                <h4>Connexion</h4>
-            </div>
-            <div class="card-body">
-                <form action="{{ path('app_login') }}" method="post">
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Nom d'utilisateur</label>
-                        <input type="text" id="username" class="form-control" name="_username" required autofocus>
-                    </div>
+use App\Kernel;
 
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Mot de passe</label>
-                        <input type="password" id="password" class="form-control" name="_password" required>
-                    </div>
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-                    <div class="Ed-flex justify-content-between align-items-center">
-                        <button type="submit" class="btn btn-primary">Se connecter</button>
-                        <a href="{{ path('app_register') }}" class="btn btn-link">S'inscrire</a>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-{% endblock %}
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,12 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig');
+        // Récupérer l’erreur de connexion s’il y en a une
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Récupérer le dernier nom d’utilisateur saisi
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Ce contrôleur sera intercepté par le système de déconnexion de Symfony.
+        throw new \Exception('Cette méthode peut être vide, elle sera interceptée par la clé de déconnexion dans votre firewall.');
     }
 }
