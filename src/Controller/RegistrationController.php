4faaf823889b83625b98512c3be061fb9960d1c1
Ivann <ivann.vasic@utbm.fr>
2025-04-03 16:34:39 +0200

    security

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,17 +1,46 @@
 <?php
+// src/Controller/RegistrationController.php
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(): Response
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $entityManager): Response
     {
-        // Pour l'instant, on peut afficher une page de test
-        return $this->render('registration/register.html.twig');
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encodage du mot de passe
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            // Sauvegarde de l'utilisateur en base
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Optionnel : connecter automatiquement l'utilisateur ou rediriger vers la page d'accueil
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
     }
 }
