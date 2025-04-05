e5fb21f662e813681e60d2c7b7c92709578b38dd
Ivann <ivann.vasic@utbm.fr>
2025-04-05 11:47:03 +0200

    Mes activites

---
+++ src/Controller/ActivitiesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ActivitiesController extends AbstractController
+{
+    #[Route('/activities', name: 'app_activities')]
+    public function index(): Response
+    {
+        return $this->render('activities.html.twig');
+    }
+}
