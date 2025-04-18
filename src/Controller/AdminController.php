5c1e45392011f86fc5e35e8d3822d3c9a8a6a843
Lucas Vigny <Lucas.vigny@gmail.com>
2025-04-19 00:21:14 +0200

    ajout de la connexion de l'admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
-        return $this->render('admin.html.twig');
+        return $this->render('admin/admin.html.twig');
     }
 }
