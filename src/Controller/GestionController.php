1178b7a11b5d079ac15f6aa4d8d170333ee12aad
Ivann <ivann.vasic@utbm.fr>
2025-04-23 11:42:13 +0200

    Correction menu

--- src/Controller/GestionController.php
+++ src/Controller/GestionController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class GestionController extends AbstractController
 {
@@ -15,4 +19,25 @@ class GestionController extends AbstractController
             'controller_name' => 'GestionController',
         ]);
     }
+    #[Route('/update-password', name: 'update_password', methods: ['POST'])]
+    public function updatePassword(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        $user = $this->getUser();
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['password']) && strlen($data['password']) >= 6) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+            $user->setPassword($hashedPassword);
+            $em->flush();
+
+            return new JsonResponse(['status' => 'success']);
+        }
+
+        return new JsonResponse(['status' => 'error', 'message' => 'Mot de passe invalide ou trop court.'], 400);
+    }
+
+
 }
