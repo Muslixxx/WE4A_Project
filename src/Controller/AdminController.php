c8d8e41cf2860f21b467ef0d453b4bffb0e1477c
Ivann <ivann.vasic@utbm.fr>
2025-04-27 13:53:15 +0200

    Page admin update avec les users/UEs de la Bdd

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Repository\CourseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +13,14 @@ class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
     #[IsGranted('ROLE_ADMIN')]
-    public function index(): Response
+    public function index(UserRepository $userRepository, CourseRepository $courseRepository): Response
     {
-        return $this->render('admin/admin.html.twig');
+        $users = $userRepository->findAll();
+        $courses = $courseRepository->findAll();
+
+        return $this->render('admin/admin.html.twig', [
+            'users' => $users,
+            'courses' => $courses,
+        ]);
     }
 }
