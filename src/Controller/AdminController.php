d6f2da22d838f4b8ec269a0921085b227394f94a
Ivann <ivann.vasic@utbm.fr>
2025-04-27 15:30:19 +0200

    Modifier User/ ajout role prof admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,6 +10,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class AdminController extends AbstractController
 {
@@ -57,4 +60,54 @@ class AdminController extends AbstractController
 
         return new JsonResponse(['status' => 'success']);
     }
+
+    #[Route('/admin/update-user/{id}', name: 'admin_update_user', methods: ['POST'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function updateUser(
+        int $id,
+        Request $request,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher
+    ): JsonResponse {
+        $user = $em->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            return new JsonResponse(['status' => 'error', 'message' => 'Utilisateur non trouvé.'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        if (isset($data['email'])) {
+            $user->setEmail($data['email']);
+        }
+        if (isset($data['firstName'])) {
+            $user->setFirstName($data['firstName']);
+        }
+        if (isset($data['lastName'])) {
+            $user->setLastName($data['lastName']);
+        }
+        if (isset($data['phoneNumber'])) {
+            $user->setPhoneNumber($data['phoneNumber']);
+        }
+        if (isset($data['birthDate']) && !empty($data['birthDate'])) {
+            try {
+                $birthDate = new \DateTime($data['birthDate']);
+                $user->setBirthDate($birthDate);
+            } catch (\Exception $e) {
+                return new JsonResponse(['status' => 'error', 'message' => 'Date de naissance invalide.'], 400);
+            }
+        }
+        if (isset($data['role'])) {
+            $user->setRole($data['role']);
+        }
+        if (!empty($data['password'])) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+            $user->setPassword($hashedPassword);
+        }
+
+        $em->flush();
+
+        return new JsonResponse(['status' => 'success']);
+    }
+
 }
