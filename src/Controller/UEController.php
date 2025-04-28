3ddb3af2794dd464e13ef93ec7253963ac22d59d
Ivann <ivann.vasic@utbm.fr>
2025-04-28 23:41:10 +0200

    Modifier page présentation

--- src/Controller/UEController.php
+++ src/Controller/UEController.php
@@ -11,7 +11,6 @@ class UEController extends AbstractController
     #[Route('/cours/{id}', name: 'cours_detail')]
     public function show(int $id): Response
     {
-        // Tu pourras plus tard récupérer le cours depuis la base
         return $this->render('UE.html.twig', [
             'id' => $id,
         ]);
